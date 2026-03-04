104e26773d7e21a0e453178e6b9bcaf1474411f6
Anniina-55 <anniina.leikas55@gmail.com>
2026-03-04 11:21:55 +0200

    Updated app theme, colors, gradle and gitignore files

--- app/src/main/java/com/example/location/ui/theme/Theme.kt
+++ app/src/main/java/com/example/location/ui/theme/Theme.kt
@@ -12,15 +12,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = LightBeetroot,
+    secondary = LightBurgundy,
+    tertiary = Light,
+    surface = LightBeetroot,
+    onSurface = Light
+
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Beetroot,
+    secondary = Burgundy,
+    tertiary = Pinkish,
+    surface = Beetroot,
+    onSurface = Burgundy,
+    onPrimary = Light,
+    onSecondary = Light,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +44,7 @@ private val LightColorScheme = lightColorScheme(
 fun LocationTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
