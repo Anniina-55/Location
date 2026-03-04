c0cbe181253e181e05480a92f5a1d64491de7b7e
Anniina-55 <anniina.leikas55@gmail.com>
2026-03-04 11:19:07 +0200

    App navigation

--- app/src/main/java/com/example/location/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/location/navigation/AppNavigation.kt
@@ -1,2 +1,52 @@
 package com.example.location.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.location.ui.components.TopAppBar
+import com.example.location.ui.screens.HomeScreen
+import com.example.location.ui.screens.LocationScreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun AppNavigation() {
+    val navController = rememberNavController()
+    val currentBackEntry = navController.currentBackStackEntryAsState()
+    val currentRoute = currentBackEntry.value?.destination?.route
+
+    val routeTitles = mapOf(
+        "home" to "Home",
+        "location" to "Location"
+    )
+    val currentTitle = routeTitles[currentRoute] ?: "App"
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = currentTitle,
+                canNavigateBack = currentRoute != "home",
+                onBackClick = { navController.popBackStack() },
+                onMenuItemClick = { route ->
+                    navController.navigate(route)
+                }
+            )}
+    ) { innerPadding ->
+        NavHost(
+            navController,
+            startDestination = "home",
+            Modifier.padding(innerPadding)
+        ) {
+            composable("home") { HomeScreen(navController) }
+            composable("location") { LocationScreen(navController) }
+        }
+    }
+}
+
